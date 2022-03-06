d6d7b6f45abd7ca0b36e2544b196c7ddedf77a4d
Martyn Lazarewicz <75955903+MartynLazarewicz@users.noreply.github.com>
2022-03-06 20:58:42 +0100

    Cloned repository with rwd files

---
+++ js/hamburger.js
@@ -0,0 +1,20 @@
+const btnHamburger = document.querySelector('#menuHamburger');
+const header = document.querySelector('.header');
+const menu = document.querySelector('.mobileMenu');
+const hamburgerMenu = document.querySelector('.nav__hamburger');
+
+btnHamburger.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    // close hamburger
+    // body.classList.remove('noscroll');
+    header.classList.remove('open');
+    menu.classList.add('is-hidden--menu');
+    hamburgerMenu.classList.remove('hamburgerOpen');
+  } else {
+    // open hamburger
+    // body.classList.add('noscroll');
+    header.classList.add('open');
+    menu.classList.remove('is-hidden--menu');
+    hamburgerMenu.classList.add('hamburgerOpen');
+  }
+});
